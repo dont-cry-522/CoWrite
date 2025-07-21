333e321aa83eba277acc18cb1ad209643ee8de9b
heathcetide <154225089+heathcetide@users.noreply.github.com>
2025-07-21 14:51:02 +0800

    feat: add h2 config and add global cors - page request

---
+++ src/main/java/com/cowrite/project/CoWriteApplication.java
@@ -0,0 +1,11 @@
+package com.cowrite.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWriteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoWriteApplication.class, args);
+    }
+}
