6163167dc8653e636c3650e0b4aa621a5e9f95d5
heathcetide <154225089+heathcetide@users.noreply.github.com>
2025-07-22 11:14:21 +0800

    feat: add ui basic config

--- ui/dist-electron/main.js
+++ ui/dist-electron/main.js
@@ -12,6 +12,9 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new BrowserWindow({
+    width: 1300,
+    height: 800,
+    autoHideMenuBar: true,
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs")
--- ui/electron/main.ts
+++ ui/electron/main.ts
@@ -28,6 +28,9 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
+    width: 1300,
+    height: 800,
+    autoHideMenuBar: true,
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
